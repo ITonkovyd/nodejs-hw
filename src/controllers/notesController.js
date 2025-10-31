a7747c1900939e8bb7be398a76891e2fee248aad
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-10-31 16:23:52 +0200

    Update schema: update schema umports 02-modgodb

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,53 +1,55 @@
-import { Notes } from "../models/notes.js";
-import createHttpError from "http-errors";
+import { Note } from '../models/note.js';
+import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Notes.find();
+	const notes = await Note.find();
 
-  res.status(200).json(notes);
+	res.status(200).json(notes);
 };
 
 export const getNoteById = async (req, res, next) => {
-  const { noteId } = req.params;
-	const note = await Notes.findById(noteId);
+	const { noteId } = req.params;
+	const note = await Note.findById(noteId);
 
 	if (!note) {
-    next(createHttpError(404, "Note not found"));
-    return;
+		next(createHttpError(404, 'Note not found'));
+		return;
 	}
 
 	res.status(200).json(note);
 };
 
 export const createNote = async (req, res) => {
-  const newNote = await Notes.create(req.body);
+	const newNote = await Note.create(req.body);
 
-  res.status(201).json(newNote);
+	res.status(201).json(newNote);
 };
 
 export const deleteNote = async (req, res, next) => {
-  const { noteId } = req.params;
+	const { noteId } = req.params;
 
-  const noteToDelete = await Notes.findByIdAndDelete(noteId);
+	const noteToDelete = await Note.findByIdAndDelete(noteId);
 
-  if (!noteToDelete) {
-    next(createHttpError(404, "Note not found"));
-    return;
-  }
+	if (!noteToDelete) {
+		next(createHttpError(404, 'Note not found'));
+		return;
+	}
 
-  res.status(200).json(noteToDelete);
+	res.status(200).json(noteToDelete);
 };
 
 export const updateNote = async (req, res, next) => {
-  const { noteId } = req.params;
-  const newNote = req.body;
+	const { noteId } = req.params;
+	const newNote = req.body;
 
-  const noteUpdate = await Notes.findByIdAndUpdate(noteId, newNote, {new: true});
+	const noteUpdate = await Note.findByIdAndUpdate(noteId, newNote, {
+		new: true,
+	});
 
-  if (!noteId) {
-    next(createHttpError(404, "Note not found"));
-    return;
-  }
+	if (!noteId) {
+		next(createHttpError(404, 'Note not found'));
+		return;
+	}
 
-  res.status(200).json(noteUpdate);
+	res.status(200).json(noteUpdate);
 };
