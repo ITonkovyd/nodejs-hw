2d0809b3a4b11d00b6cb907da6533beba8d5418f
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-11-03 12:01:39 +0200

    Update controller: changing param naming 03-validation

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -18,17 +18,17 @@ export const getAllNotes = async (req, res) => {
 		notesQuery.where('tag').eq(tag);
 	}
 
-	const [totalItems, notes] = await Promise.all([
+	const [totalNotes, notes] = await Promise.all([
 		notesQuery.clone().countDocuments(),
 		notesQuery.skip(skipCount).limit(perPage),
 	]);
 
-	const totalPages = Math.ceil(totalItems / perPage);
+	const totalPages = Math.ceil(totalNotes / perPage);
 
 	res.status(200).json({
 		page,
 		perPage,
-		totalItems,
+		totalNotes,
 		totalPages,
 		notes,
 	});
