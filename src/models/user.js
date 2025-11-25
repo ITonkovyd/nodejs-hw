d43b69f737f848ee16296566febee48c339881ab
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-11-25 22:40:23 +0200

    add model: include avatar field in user schema 05-img

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,32 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    trim: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 8
-  }},
-  {
-    timestamps: true,
-    versionKey: false,
-  }
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			trim: true,
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minLength: 8,
+		},
+		avatar: {
+			type: String,
+			required: false,
+			default: 'https://ac.goit.global/fullstack/react/default-avatar.jpg',
+		},
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	},
 );
 
 userSchema.pre("save", function (next) {
